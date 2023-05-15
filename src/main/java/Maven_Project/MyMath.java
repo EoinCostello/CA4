d69511552ad376f23bdfa08f18c748e69daded70
EoinCostello <123333973+EoinCostello@users.noreply.github.com>
2023-05-15 19:48:51 +0100

    Create MyMath.java

---
+++ src/main/java/Maven_Project/MyMath.java
@@ -0,0 +1,17 @@
+package Maven_Project;
+
+public class MyMath {
+	public void myAdd(int a, int b) {
+        // add Sum here
+	}
+    public void mySubtract(int a, int b) {
+        // add Sum here
+    }
+    public void myMultiply(int a, int b) {
+        // add Sum here
+    }
+    public void myDivide(int a, int b) {
+        // add Sum here
+    }
+
+}
