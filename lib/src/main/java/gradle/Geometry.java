eaee7efb5a5bf587dd6bf28849cb8fd6d782d91e
EoinCostello <123333973+EoinCostello@users.noreply.github.com>
2023-05-21 16:09:41 +0100

    Add files via upload

---
+++ lib/src/main/java/gradle/Geometry.java
@@ -0,0 +1,25 @@
+package gradle;
+
+public class Geometry {
+
+    public float areaRect(float width, float height) {
+        // areaRect() method
+        return 0;
+    }
+
+    public float perimeterRect(float width, float height) {
+        // perimeterRect() method
+        return 0;
+    }
+
+    public float volumeSphere(float radius) {
+        // volumeSphere() method
+        return 0;
+    }
+
+    public float surfaceAreaSphere(float radius) {
+        // surfaceAreaSphere() method
+        return 0;
+    }
+}
+
---
+++ lib/src/main/java/gradle/GeometryTest.java
@@ -0,0 +1,28 @@
+package gradle;
+
+import org.testng.annotations.Test;
+
+public class GeometryTest {
+
+	
+    @Test
+    public void testAreaRect() {
+        // Add test case 
+    }
+
+    @Test
+    public void testPerimeterRect() {
+        // Add test case
+    }
+  
+    @Test
+    public void testVolumeSphere() {
+        // Add test case
+    }
+
+    @Test
+    public void testSurfaceAreaSphere() {
+        // Add test case
+    }
+
+}
